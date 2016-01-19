6d3e17c2c3897463aed58a04242dffde7a2861c2
auxoworks <tjlee815@gmail.com>
2016-01-19 09:50:03 -0800

    finished

--- GoogleBillboard.pde
+++ GoogleBillboard.pde
@@ -1,14 +1,29 @@
 public final static String e = "2.718281828459045235360287471352662497757247093699959574966967627724076630353547594571382178525166427427466391932003059";  
+
 public void setup()  
-{            
-     noLoop();  
+{           	
+noLoop();
 }  
 public void draw()  
 {   
-	//not needed for this assignment
-}  
-public boolean isPrime(double dNum)  
-{   
-    //to be finished later   
-    return true;  
+	for(int i=0;i<e.length()-1;i++){
+ double dNum = Double.parseDouble(e.substring(i,i+10));
+	if (isPrime(dNum)==true&&dNum%1==0){
+ System.out.println(dNum);
+}
+}
+ }  
+public boolean isPrime(double dNum)   
+  {
+  if (dNum==1)
+  return false;
+  else if (dNum==2) 
+  return true;
+  if (dNum<0)
+  return false;
+  for( int i=2; i<=Math.sqrt(dNum); i++){
+  if (dNum%i==0)
+  return false;
+ }
+  return true; 
 } 
---
+++ build-tmp/source/GoogleBillboard.java
@@ -0,0 +1,54 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class GoogleBillboard extends PApplet {
+
+public final static String e = "2.718281828459045235360287471352662497757247093699959574966967627724076630353547594571382178525166427427466391932003059";  
+
+public void setup()  
+{           	
+noLoop();
+}  
+public void draw()  
+{   
+	for(int i=0;i<e.length()-1;i++){
+ double dNum = Double.parseDouble(e.substring(i,i+10));
+	if (isPrime(dNum)==true&&dNum%1==0){
+ System.out.println(dNum);
+}
+}
+ }  
+public boolean isPrime(double dNum)   
+  {
+  if (dNum==1)
+  return false;
+  else if (dNum==2) 
+  return true;
+  if (dNum<0)
+  return false;
+  for( int i=2; i<=Math.sqrt(dNum); i++){
+  if (dNum%i==0)
+  return false;
+ }
+  return true; 
+} 
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "GoogleBillboard" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
